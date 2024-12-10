d8b53ce7597f1049e309aa2ccc1932216ba49101
swallowedB <musamea99@gmail.com>
2024-12-10 20:15:07 +0900

    ✨ feat: CSS 커스텀 파일 추가 및 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // 폰트
+      fontFamily: {
+        pretendard: ['Pretendard', 'san-serif'],
+        rubik: ['"Rubik Bubbles"', 'cursive'],
+      },
+      // 커스텀 컬러
+      colors: {
+        primary: {
+          50: '#E8F0FA',
+          100: '#d8e7f9',
+          200: '#B1D0F4',
+          300: '#8AB8EE',
+          400: '#63A1E8',
+          500: '#d8e7f9',
+          600: '#306EB5',
+          700: '#245288',
+          800: '#18375b',
+          900: '#0c1b2d',
+        },
+        custom: {
+          gray: '#7d848d',
+          black: '#2e2e2e',
+          graylight: '#D7D7D7',
+          input: '#FEFEFE'
+        }
+      },
+      // 커스텀 그림자
+      boxShadow: {
+        'default': '0px 8px 18px 0px rgba(168, 178, 198, 0.45)',
+        'backblue': '0px 4px 10px 0px rgba(49, 78, 141, 0.25);',
+        'blue': '0px 4px 10px 0px rgba(180, 184, 201, 0.40)',
+      }
+
+    },
   },
   plugins: [],
   future: {
