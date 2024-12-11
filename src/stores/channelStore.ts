073bb7f1b1a6f460c9420d30ad69b00817e1addb
nirii00 <nirii00@users.noreply.github.com>
2024-12-11 22:59:48 +0900

    ✨ Feature: 카테고리 퍼블리싱 완료, 카테고리 기능 구현

---
+++ src/stores/channelStore.ts
@@ -0,0 +1,79 @@
+import { create } from "zustand";
+
+type ChannelType = {
+  authRequired: boolean;
+  posts: string[];
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+interface ChannelStore {
+  isMenuClicked: { location: boolean; spot: boolean };
+  location: string;
+  spot: string;
+  channelList: ChannelType[];
+  locationList: ChannelType[];
+  spotList: ChannelType[];
+  toggleMenu: (menu: "location" | "spot") => void;
+  setLocation: (location: string) => void;
+  setSpot: (spot: string) => void;
+  setChannelList: (channels: ChannelType[]) => void;
+  setLocationList: (channels: ChannelType[]) => void;
+  setSpotList: (channels: ChannelType[]) => void;
+}
+
+export const useChannelStore = create<ChannelStore>((set) => ({
+  isMenuClicked: { location: false, spot: false },
+  location: "서울",
+  spot: "전체",
+  channelList: [],
+  locationList: [],
+  spotList: [],
+
+  // 메뉴 상태를 토글
+  toggleMenu: (menu) =>
+    set((state) => ({
+      isMenuClicked: {
+        location: menu === "location" ? !state.isMenuClicked.location : false,
+        spot: menu === "spot" ? !state.isMenuClicked.spot : false,
+      },
+    })),
+
+  // 위치 설정
+  setLocation: (location) =>
+    set({
+      location,
+      isMenuClicked: {
+        location: false,
+        spot: false,
+      },
+    }),
+
+  // 스팟 설정
+  setSpot: (spot) =>
+    set({
+      spot,
+      isMenuClicked: {
+        location: false,
+        spot: false,
+      },
+    }),
+
+  // 전체 채널 리스트 설정
+  setChannelList: (channels) => set({ channelList: channels }),
+
+  // 지역 리스트 설정
+  setLocationList: (channels) =>
+    set(() => ({
+      locationList: channels.filter((ch) => ch.description === "지역"),
+    })),
+
+  // 스팟 리스트 설정
+  setSpotList: (channels) =>
+    set(() => ({
+      spotList: channels.filter((ch) => ch.description === "스팟"),
+    })),
+}));
