0512241ab02d4cee77b33a53f9fa245847a7e2f5
naehyun <scott253@naver.com>
2024-12-11 15:13:46 +0900

    ♻️ RootLayout.tsx의 불필요한 코드 제거, Nav.tsx에서 코드 수정하여 로그인 유지 기능 구현

--- src/components/Root/Nav.tsx
+++ src/components/Root/Nav.tsx
@@ -1,30 +1,26 @@
-import { useState } from "react";
 import FooterNavLink from "../FooterNavLink";
+import { useAuthStore } from "../../stores/authStore";
 
 export default function Nav() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   return (
     <footer className="relative w-full h-20 bg-white border-t shadow-nav border-zinc-100 ">
-    <div className="absolute inset-0 flex items-center justify-center">
-      <nav className="relative flex items-center justify-around w-full h-full max-w-3xl px-4">
-        <FooterNavLink
-          to="/category"
-          icon="category_icon"
-          label="카테고리"
-        />
-        <FooterNavLink to="/" icon="feed_icon" label="피드" />
-        <FooterNavLink
-          to="/createCourse"
-          icon="create_icon"
-          label="코스생성"
-        />
-        <FooterNavLink
-          to={isLoggedIn ? "/my-page" : "/login"}
-          icon="mypage_icon"
-          label="마이페이지"
-        />
-      </nav>
-    </div>
-  </footer>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <nav className="relative flex items-center justify-around w-full h-full max-w-3xl px-4">
+          <FooterNavLink to="/category" icon="category_icon" label="카테고리" />
+          <FooterNavLink to="/" icon="feed_icon" label="피드" />
+          <FooterNavLink
+            to="/createCourse"
+            icon="create_icon"
+            label="코스생성"
+          />
+          <FooterNavLink
+            to={isLoggedIn ? "/my-page" : "/login"}
+            icon="mypage_icon"
+            label="마이페이지"
+          />
+        </nav>
+      </div>
+    </footer>
   );
 }
