7f94f2bb92bb299873ef6034a525e559e79d5732
swallowedB <musamea99@gmail.com>
2024-12-12 19:56:24 +0900

    ✨ feat: 마이페이지 퍼블리싱 및 자잘한 CSS 수정

---
+++ src/components/main/My/MypageCards/MypageCardItem.tsx
@@ -0,0 +1,61 @@
+import img from "../../../../assets/images/tryImg.png"
+import images from "../../../../assets/images/importImages"
+
+export default function MypageCardItem() {
+  return (
+    <div>
+      <div className={`
+        w-[177px] h-[194px] rounded-2xl bg-white shadow-blue
+        relative flex flex-col items-center p-2
+        `}>
+        <div className="absolute top-[-2px] right-[8px]">
+          {/* 좋아요 버튼 */}
+          <div className={`
+            absolute flex items-center justify-center 
+            bg-custom-black/20 rounded-full top-[18px] right-[8.71px] 
+            h-7 w-7 cursor-pointer`}>
+            <img
+              src={images.like_icon}
+              alt="Like Icon"
+              className="h-[13px] w-[14px]"
+            />
+          </div>
+        </div>
+
+        {/* 썸네일 이미지 */}
+        <div
+          style={{
+            backgroundImage: `url(${img})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat"
+          }} 
+          className="w-[158px] h-[125px] " />
+        
+        {/* card texts */}
+        
+        {/* 제목 및 좋아요 수 */}
+        <div className="flex flex-row items-center justify-between gap-[10px] mt-[9px] w-[154px]">
+          <p className="font-pretendard text-[14px] font-regular text-custom-black">2025 새해 모임</p>
+          {/* 좋아요 수 */}
+          <div className="flex flex-row items-center">
+            <img src={images.like_filled_icon} alt="좋아요 아이콘" />
+            <p className="ml-[2px] font-pretendard text-[13px] font-regular text-custom-black">4.7k</p>
+          </div>
+        </div>
+        
+        {/* 위치 */}
+        <div className="flex flex-row items-center mt-[3px] w-[154px] ">
+          {/* 주소 아이콘 */}
+          <img
+            src={images.location_icon}
+            alt="Location Icon"
+            className="w-4 h-4 mr-1"
+          />
+          <p className="font-pretendard text-[12px] text-custom-gray font-regular">Seoul, GangNam</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
