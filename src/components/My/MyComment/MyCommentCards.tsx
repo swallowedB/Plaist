f19c3367dc3b9f22dcf920b09cf01dd9d51637c8
swallowedB <musamea99@gmail.com>
2024-12-13 15:05:18 +0900

    ♻️ refact: 마이페이지 퍼블리싱 및 검색바 수정

--- src/components/My/MyComment/MyCommentCards.tsx
+++ src/components/My/MyComment/MyCommentCards.tsx
@@ -4,7 +4,7 @@ import MyCommentCardItem from "./MyCommentCardItem";
 export default function MyCommentCards() {
   return (
     <div>
-      <div className="grid grid-cols-1 gap-5 ">
+      <div className="grid grid-cols-1 gap-5 hover:cursor-pointer">
         <MyCommentCardItem />
       </div>
     </div>
