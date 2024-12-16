1a4a4b33b5cccef4ed1a85c7f9f87e3df3488404
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-12-16 15:07:16 +0900

    ✨ feat: getTokenWithCloser.ts 파일 예외 처리구문 추가

--- src/utills/Auth/getTokenWithCloser.ts
+++ src/utills/Auth/getTokenWithCloser.ts
@@ -2,6 +2,10 @@ import { getCookie } from "./getCookie";
 
 const createTokenManager = () => {
   const TOKEN = getCookie("token");
+  if (!TOKEN) {
+    alert("로그인 후 이용해 주세요.");
+    return null;
+  }
 
   return {
     getToken: () => {
@@ -12,5 +16,5 @@ const createTokenManager = () => {
 
 export const getToken = () => {
   const tokenManager = createTokenManager();
-  return tokenManager.getToken(); // 토큰 값을 출력
+  return tokenManager?.getToken(); // 토큰 값을 출력
 };
