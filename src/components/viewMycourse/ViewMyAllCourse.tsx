64e69af2de1544e582ded1072efc957813068cf5
naehyun <scott253@naver.com>
2024-12-16 20:41:46 +0900

    마이코스 필터링

--- src/components/viewMycourse/ViewMyAllCourse.tsx
+++ src/components/viewMycourse/ViewMyAllCourse.tsx
@@ -4,19 +4,26 @@ import MyCourseSearch from "./MyCourseSearch";
 import MyCourseCards from "./MyCourseCards";
 import { getUserIdFromToken } from "../../api/userApi";
 
-const allCourseChannelId = getUserIdFromToken();
 // 여기에 내 코스만 필터되도록 넣어놓으셈
 // 내 코스만 필터된 것에서 서치바 검색이 감지되면 mycourse가 저장된 것(메모이제이션 사용)에서 다시 필터
 
 export default function ViewMyAllCourse() {
   const [courseList, setCourseList] = useState([]);
+  const [userId, setUserId] = useState("");
+
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await getMyCourseObj(allCourseChannelId);
-      setCourseList(res);
-    };
-    fetchData();
+    const currentUserId = getUserIdFromToken();
+    setUserId(currentUserId);
   }, []);
+  useEffect(() => {
+    if (userId) {
+      const fetchData = async () => {
+        const res = await getMyCourseObj(userId);
+        setCourseList(res);
+      };
+      fetchData();
+    }
+  }, [userId]); // userId가 변경될 때마다 실행
 
   return (
     <div className="flex flex-col items-center justify-center">
