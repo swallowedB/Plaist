5291adf9d4f4b3ad6c22d081dd529cefee2d82c3
naehyun <scott253@naver.com>
2024-12-16 20:41:29 +0900

    마이코스 필터링

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,10 +1,9 @@
 import { getCookie } from "../utills/Auth/getCookie";
-import { axiosInstance } from "./axios"
+import { axiosInstance } from "./axios";
 import { jwtDecode } from "jwt-decode";
 
-
 type JwtPayload = {
-  user: { 
+  user: {
     _id: string;
     email: string;
   };
@@ -12,19 +11,19 @@ type JwtPayload = {
 };
 
 // JWT에서 userId 추출 함수
-const getUserIdFromToken = () => {
-  const token = getCookie("token"); 
+export const getUserIdFromToken = () => {
+  const token = getCookie("token");
   if (!token) {
     throw new Error("사용자 토큰이 존재하지 않습니다.");
   }
   const decodedToken = jwtDecode<JwtPayload>(token);
-  console.log('Decoded JWT:', decodedToken); // 디코딩된 토큰 확인
+  console.log("Decoded JWT:", decodedToken); // 디코딩된 토큰 확인
   return decodedToken.user._id; // 서버의 JWT 구조에 따라 키 확인
 };
 
 // 사용자 정보 불러오기
 export const getUserInfo = async () => {
-  try{
+  try {
     const userId = getUserIdFromToken();
     console.log(userId);
     const response = await axiosInstance.get(`/users/${userId}`);
@@ -36,15 +35,19 @@ export const getUserInfo = async () => {
 };
 
 // 사용자 정보 변경
-export const updateUserInfo = async ({ fullName, email }: { fullName: string; email: string }) => {
-  try{
-    const response = await axiosInstance.put(`/settings/update-user`,
-      {
-        headers: { 'Cache-Control': 'no-cache' },
-        fullName,
-        email
-      }
-    );
+export const updateUserInfo = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  try {
+    const response = await axiosInstance.put(`/settings/update-user`, {
+      headers: { "Cache-Control": "no-cache" },
+      fullName,
+      email,
+    });
     return response.data;
   } catch (error) {
     console.error("/settings/update-user 호출 중 오류 발생:", error);
@@ -53,14 +56,14 @@ export const updateUserInfo = async ({ fullName, email }: { fullName: string; em
 
 // 사용자 프로필 사진 변경
 export const postUserPoto = async (formData: FormData) => {
-  try{
+  try {
     const response = await axiosInstance.post(`/users/upload-photo`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
 
-    const { image } =response.data;
+    const { image } = response.data;
     console.log("업로드된 이미지 URL Check:", image);
     return image;
   } catch (error) {
--- src/components/viewMycourse/ViewMyAllCourse.tsx
+++ src/components/viewMycourse/ViewMyAllCourse.tsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
-import { getChannelPostList } from "../../api/postApi";
-import { useQuery } from "@tanstack/react-query";
-
+import { getMyCourseObj } from "../../api/postApi";
 import MyCourseSearch from "./MyCourseSearch";
 import MyCourseCards from "./MyCourseCards";
+import { getUserIdFromToken } from "../../api/userApi";
 
-const allCourseChannelId = `675e6ed26ada400ee6bec120`;
-  // 여기에 내 코스만 필터되도록 넣어놓으셈
-  // 내 코스만 필터된 것에서 서치바 검색이 감지되면 mycourse가 저장된 것(메모이제이션 사용)에서 다시 필터
+const allCourseChannelId = getUserIdFromToken();
+// 여기에 내 코스만 필터되도록 넣어놓으셈
+// 내 코스만 필터된 것에서 서치바 검색이 감지되면 mycourse가 저장된 것(메모이제이션 사용)에서 다시 필터
+
 export default function ViewMyAllCourse() {
   const [courseList, setCourseList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const res = await getChannelPostList(allCourseChannelId);
+      const res = await getMyCourseObj(allCourseChannelId);
       setCourseList(res);
     };
     fetchData();
   }, []);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <MyCourseSearch />
