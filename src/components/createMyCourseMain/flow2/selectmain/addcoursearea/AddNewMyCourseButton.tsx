d43c68e22fe2ad52249f9e86fe27626eb7a87395
engsal <imaria0218@gmail.com>
2024-12-17 19:32:12 +0900

    🐛 fix: 버튼과 슬라이더바 - 단위 수정-nav바 숨김 버튼 경로 조정

--- src/components/createMyCourseMain/flow2/selectmain/addcoursearea/AddNewMyCourseButton.tsx
+++ src/components/createMyCourseMain/flow2/selectmain/addcoursearea/AddNewMyCourseButton.tsx
@@ -10,7 +10,7 @@ export default function AddNewMyCourseButton({
   return (
     <button
       onClick={() => onPlus()}
-      className="w-[529px] h-[108px] flex items-center justify-center rounded-[21px] bg-primary-50 border-dashed border-primary-500 mt-[38px]"
+      className="w-[529px] h-[108px] flex items-center justify-center rounded-[21px] bg-primary-50 border-dashed border-primary-500 mt-[34px]"
     >
       <img src={images.plus_icon} alt="plus-icon" />
     </button>
