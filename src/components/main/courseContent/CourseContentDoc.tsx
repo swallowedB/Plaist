850e466c6577ed7a3e6b9d29278e8b1b4ed4d3fc
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-12-20 09:57:41 +0900

    🐛 fix: 상세페이지 뱃지 사이즈 조절

--- src/components/main/courseContent/CourseContentDoc.tsx
+++ src/components/main/courseContent/CourseContentDoc.tsx
@@ -112,7 +112,7 @@ export default function CourseContentDoc({
             <p>{doc.courseDescription}</p>
           </div>
 
-          <div className="flex gap-[14px] h-6 mb-10">
+          <div className="flex gap-[14px] h-6 mb-5">
             <div className="flex items-center gap-[11px]">
               <img
                 src={images.course_estimated_time_icon}
@@ -142,7 +142,7 @@ export default function CourseContentDoc({
         <CourseBadge target={doc.withWhom} />
         <CourseBadge target={doc.style} />
       </div>
-
+      
       <CourseLocationCards doc={doc} />
       <CourseContentCommentArea courseObj={courseObj} />
     </div>
