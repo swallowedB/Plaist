4bc1b76ce4667e6e528a3ea85287a75c52066610
naehyun <scott253@naver.com>
2024-12-20 09:10:29 +0900

    userApi.ts 오류 useIsLoginStore.ts 수정, 마이코스 페이지 css 수정, 카테고리 화살표 머리 방향 수정, CardData 타입 병합, getMyCourseObj 수정(필터링)

--- src/components/My/MypageMyCourse.tsx
+++ src/components/My/MypageMyCourse.tsx
@@ -4,35 +4,25 @@ import MypageCards from "./MypageCards/MypageCards";
 import { useUserStore } from "../../stores/useInfoStore";
 import { useMyCourseStore } from "../../stores/useMyCourseStore";
 
-type CardData = {
-  id: string;
-  courseTitle: string;
-  courseDescription: string;
-  locationAddress: string;
-  likes: number;
-  image: string;
-};
-
 export default function MypageMyCourse() {
   const { myCourseList, fetchMyCourses } = useMyCourseStore();
-  const [ filteredList, setFilteredList ] = useState<CardData[]>([]);
+  const [filteredList, setFilteredList] = useState<CardData[]>([]);
   const [isSearching, setIsSearching] = useState(false);
   const { userId, setUserId } = useUserStore();
 
   useEffect(() => {
-    if(!userId){
+    if (!userId) {
       setUserId();
     }
-    if(userId) {
+    if (userId) {
       fetchMyCourses(userId);
     }
-  }, [userId, setUserId, fetchMyCourses])
+  }, [userId, setUserId, fetchMyCourses]);
 
-  
-    const handleSearch = (result: CardData[]) => {
-      setIsSearching(result.length > 0 || result.length === 0);
-      setFilteredList(result);
-    }
+  const handleSearch = (result: CardData[]) => {
+    setIsSearching(result.length > 0 || result.length === 0);
+    setFilteredList(result);
+  };
 
   return (
     <div className={`flex flex-col items-center`}>
@@ -43,27 +33,27 @@ export default function MypageMyCourse() {
         onSearch={handleSearch}
         placeholder="무엇이든 검색해보세요 (oﾟvﾟ)ノ"
       />
-      
+
       {/* 데이터 렌더링 */}
-      <div className="mt-8 flex flex-col">
+      <div className="flex flex-col mt-8">
         {isSearching ? (
           filteredList.length > 0 ? (
             <MypageCards data={filteredList} />
           ) : (
-            <div className="col-span-3 mt-10 font-semiBold text-center text-primary-700 font-pretendard text-sm">
+            <div className="col-span-3 mt-10 text-sm text-center font-semiBold text-primary-700 font-pretendard">
               검색 결과를 찾지 못했어요 ψ(´´∀´´)ψ
             </div>
           )
+        ) : myCourseList.length > 0 ? (
+          <MypageCards data={myCourseList} />
+        ) : myCourseList.length > 0 ? (
+          <MypageCards data={myCourseList} />
         ) : (
-          myCourseList.length > 0 ? (
-            <MypageCards data={myCourseList} />
-          ) : (
-            <div className="col-span-3 mt-10 font-medium text-center text-primary-700 font-pretendard text-sm">
-              아직 등록된 코스가 없어요 இ௰இ
-            </div>
-          )
+          <div className="col-span-3 mt-10 font-medium text-center text-primary-700 font-pretendard text-sm">
+            아직 등록된 코스가 없어요 இ௰இ
+          </div>
         )}
       </div>
     </div>
-  )
+  );
 }
