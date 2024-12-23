f3d0fb7f1bbf6972ef346eec10336c26ddeb0ed7
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-12-23 17:37:52 +0900

    hotfix: locator 비활성화

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
-if (process.env.NODE_ENV === "development") {
-  setupLocatorUI();
-}
+// if (process.env.NODE_ENV === "development") {
+//   setupLocatorUI();
+// }
 createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
